030c8c01b47edcdd8783db3a5161edd70f058a47
Negasi-Kifle <kiflenegasi@yahoo.com>
2024-02-28 12:50:11 +0300

    Minor bug in home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,16 @@ import Layout from "./layout";
 import Home from "./pages/Home";
 import AddSong from "./pages/AddSong";
 import Analytics from "./pages/Analytics";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { GET_SONGS } from "./redux/sagas/types";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({ type: GET_SONGS });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
